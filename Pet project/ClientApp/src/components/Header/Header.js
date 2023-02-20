98bb217f584a1eced0e33b020580fefcfd0e0d1f
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2023-02-20 09:53:52 +0400

    add some content and styles for header component, also add variables to control style values in project

--- Pet project/ClientApp/src/components/Header/Header.js
+++ Pet project/ClientApp/src/components/Header/Header.js
@@ -3,7 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
 require("../../style/components/_Header.scss");
 var Header = function () {
-    return (React.createElement("header", null));
+    return (React.createElement("header", null,
+        React.createElement("div", { className: "d-flex justify-content-around" },
+            React.createElement("ul", { className: "list-group list-group-horizontal justify-content-between info-list" },
+                React.createElement("li", { className: "list-group-item flex-fill info-list__item" }, "\u041E \u043A\u043E\u043C\u043F\u0430\u043D\u0438\u0438"),
+                React.createElement("li", { className: "list-group-item flex-fill info-list__item" }, "\u0414\u043E\u0441\u0442\u0430\u0432\u043A\u0430"),
+                React.createElement("li", { className: "list-group-item flex-fill info-list__item" }, "\u041D\u043E\u0432\u043E\u0441\u0442\u0438"),
+                React.createElement("li", { className: "list-group-item flex-fill info-list__item" }, "\u0410\u043A\u0446\u0438\u0438"),
+                React.createElement("li", { className: "list-group-item flex-fill info-list__item" }, "\u0423\u0441\u043B\u0443\u0433\u0438")),
+            React.createElement("button", { type: "button", className: "btn btn-primary info-btn" }, "\u0412\u043E\u0439\u0442\u0438"))));
 };
 exports.default = Header;
 //# sourceMappingURL=Header.js.map
--- Pet project/ClientApp/src/components/Header/Header.tsx
+++ Pet project/ClientApp/src/components/Header/Header.tsx
@@ -3,7 +3,18 @@ import '../../style/components/_Header.scss';
 
 const Header: React.FC<{}> = () => {
     return (
-        <header></header>    
+        <header>
+            <div className="d-flex justify-content-around">
+                <ul className="list-group list-group-horizontal justify-content-between info-list">
+                    <li className="list-group-item flex-fill info-list__item">О компании</li>
+                    <li className="list-group-item flex-fill info-list__item">Доставка</li>
+                    <li className="list-group-item flex-fill info-list__item">Новости</li>
+                    <li className="list-group-item flex-fill info-list__item">Акции</li>
+                    <li className="list-group-item flex-fill info-list__item">Услуги</li>
+                </ul>
+                <button type="button" className="btn btn-primary info-btn">Войти</button>
+            </div>
+        </header>
     )
 }
 

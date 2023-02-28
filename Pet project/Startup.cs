e19cdd92c269a8420ce26aa0757a6c37463a320c
IvanPavlov90 <59957767+IvanPavlov90@users.noreply.github.com>
2023-02-28 12:24:43 +0400

    link automapperweb to services

--- Pet project/Startup.cs
+++ Pet project/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pet_project.Common;
 using Pet_project.Data.Common;
 using Pet_project.Data.Contexts;
 
@@ -30,7 +31,7 @@ namespace Pet_project
                 configuration.RootPath = "ClientApp/build";
             });
 
-            services.AddAutoMapper(typeof(AutoMapperProfile));
+            services.AddAutoMapper(typeof(AutoMapperProfile), typeof(AutoMapperWebProfile));
 
             services.AddDbContext<PetProjectDbContext>(options => {
                 options.UseSqlServer(Configuration.GetConnectionString("PetProjectDb"));
